ba18e1e5b7f4f46a371e8eb14d87d69a38f61568
Castaldo Laetitia <laetitia.castaldo@etu.univ-lyon1.fr>
2023-04-24 11:28:59 +0200

    Youhou ! on a l'ecran mais pas encore l'id

--- tp1/code-detecteur-JS/mqtt-client-sub.js
+++ tp1/code-detecteur-JS/mqtt-client-sub.js
@@ -6,24 +6,28 @@ const client = mqtt.connect('mqtt://192.168.78.96:3306')
 
 const topic = 'iot/arduino'
 
+var lcd
+
 board.on('ready', function () {
-  const lcd = new five.LCD({
+  lcd = new five.LCD({
     pins: [12, 11, 5, 4, 3, 2],
     rows: 2,
     cols: 16
   })
+})
 
-  client.on('message', function (topicR, message) {
-    console.log(topicR + ': ', message.toString())
-    lcd.clear()
-    lcd.cursor(0, 0).print(message.toString()) // Affiche le message sur la première ligne de l'écran LCD
-  })
+client.on('message', function (topicR, message) {
+ // console.log(topicR + ': ', message.toString())
+  const json = message.toJSON()
+  console.log(json)
+  lcd.clear()
+  lcd.cursor(0, 0).print(message.toString()) // Affiche le message sur la première ligne de l'écran LCD
+})
 
-  client.on('connect', function () {
-    client.subscribe(topic, function (err) {
-      if (!err) {
-        console.log('Subscribed to:', topic)
-      }
-    })
+client.on('connect', function () {
+  client.subscribe(topic, function (err) {
+    if (!err) {
+      console.log('Subscribed to:', topic)
+    }
   })
 })
