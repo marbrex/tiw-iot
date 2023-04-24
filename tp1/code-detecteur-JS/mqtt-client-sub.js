929d1f4534bbdcb823386d5cd463445bc4af3795
Castaldo Laetitia <laetitia.castaldo@etu.univ-lyon1.fr>
2023-04-24 10:46:17 +0200

    com des parties johnny five

--- tp1/code-detecteur-JS/mqtt-client-sub.js
+++ tp1/code-detecteur-JS/mqtt-client-sub.js
@@ -1,28 +1,25 @@
 import mqtt from 'mqtt'
 import five from 'johnny-five'
 
-const board = new five.Board()
+//const board = new five.Board()
 const client = mqtt.connect('mqtt://192.168.78.96:3306')
 
 const topic = 'iot/arduino'
 
-board.on('ready', function () {
-  const rfid = new five.RFID({
-    controller: 'PN532_I2C',
-    // Attention ici il faut bien vérifier que l'adresse du PN532 est la bonne
-    // Elle peut changer en fonction du module utilisé
-    address: 0x48
-  })
+//board.on('ready', function () {
+  //const rfid = new five.RFID({
+   //test
+  //})
 
-  rfid.on('tag', function (id) {
-    console.log('ID:', id)
-    client.publish(topic, id)
-  })
+  //rfid.on('tag', function (id) {
+  //  console.log('ID:', id)
+  //  client.publish(topic, id)
+  //})
 
-  rfid.on('error', function (err) {
-    console.error('Error:', err)
-  })
-})
+ // rfid.on('error', function (err) {
+  //  console.error('Error:', err)
+ // })
+//})
 
 client.on('connect', function () {
   client.subscribe(topic, function (err) {
