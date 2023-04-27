3d52f92dc7ef689928d13c64f020457bc7839408
Castaldo Laetitia <laetitia.castaldo@etu.univ-lyon1.fr>
2023-04-27 16:55:05 +0200

    Ajout mécanique LED

--- tp1/code-detecteur-JS/mqtt-client-sub.js
+++ tp1/code-detecteur-JS/mqtt-client-sub.js
@@ -6,7 +6,7 @@ const client = mqtt.connect('mqtt://192.168.78.96:3306')
 
 const topic = 'iot/arduino'
 
-var lcd, servo, greenLight, redLight
+var lcd, servo, greenLight, redLight, ir
 
 board.on('ready', function () {
   // Déclaration de l'écran LCD
@@ -17,18 +17,22 @@ board.on('ready', function () {
   })
 
   // A verifier quels pins sont utilisées pour les leds
- // greenLed = new five.Led(9)
- // redLed = new five.Led(10)
+  greenLight = new five.Led(13)
+  redLight = new five.Led(0)
 
   //Déclaration du moteur pas à pas
   servo = new five.Servo({
     pin: 9,
     startAt: 0,
     range: [0, 180]
-    })
+  })
 
-  // Initialisation de la position du switch
-  let switchPosition = 0
+  // Déclaration du capteur infrarouge
+  ir = new five.Proximity({
+    controller: "HCSR04",
+    pin: 1,
+    freq: 200
+  });
 
   // Initialisation de la position du servo moteur
   servo.to(100)
@@ -48,32 +52,45 @@ client.on('message', function (topicR, message) {
   let orientation = Math.floor(Math.random() * 3)
   console.log(orientation)
 
+  var destination = ""
+
   // Détection d'un bagage
   if (messageStr != null) {
-   // greenLight.on()
-   // redLight.off()
 
+    greenLight.on()
+    redLight.off()
 
     servo.to(0)
     // Oriente le servo moteur dans la bonne position
     if (orientation == 0) { // RIGHT
+      destination = "Lyon"
       servo.to(200)
-    } else if (messageStr == 1) { // LEFT
-      switchPosition = 90
+    } else if (orientation == 1) { // LEFT
+      destination = "Seoul"
       servo.to(400)
-    } else if (messageStr == 2) { // DISPUTE
-      switchPosition = 180
+    } else if (orientation == 2) { // DISPUTE
+      destination = "Rien"
       servo.to(600)
     }
 
+    ir.on("data", function() {
+      const distance = this.cm;
+      console.log(distance);
+    });
+
     // Affiche la position du switch sur l'écran LCD
-    lcd.clear().print(orientation.toString())
+    lcd.clear().print(destination)
 
-  //  greenLight.off()
-  //  redLight.on()
+    setTimeout(() => {
+      
+      greenLight.off()
+      redLight.on()
+      servo.to(0)
+
+    }, 2000);
   }
-
-  // Afficher le message sur l'écran LCD  
+    
+    // Afficher le message sur l'écran LCD  
   //lcd.clear()
   //lcd.cursor(0, 0).print(message.toString()) // Affiche sur la première ligne de l'écran LCD
 })
