1b661c366f67ca918197f3877784f8d114d4968d
Castaldo Laetitia <laetitia.castaldo@etu.univ-lyon1.fr>
2023-06-16 14:27:53 +0200

    Refactoring du projet

--- tp1/code-detecteur-JS/mqtt-client-pub.js
+++
@@ -1,8 +0,0 @@
-import * as mqtt from "mqtt";
-const client = mqtt.connect("mqtt://192.168.78.96:3306");
-const topic = "iot/arduino";
-
-client.on("connect", function () {
-  client.publish(topic, "Hello mqtt");
-  client.end();
-});
