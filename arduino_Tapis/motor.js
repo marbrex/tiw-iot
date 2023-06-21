2bbb4ea378e5eb3eb78740cf404bfcd129df9b02
Castaldo Laetitia <laetitia.castaldo@etu.univ-lyon1.fr>
2023-06-21 14:47:29 +0200

    Modif code pour integration Node Red)

--- arduino_Tapis/motor.js
+++ arduino_Tapis/motor.js
@@ -1,120 +1,144 @@
-/**
- * zoetrope.js
- * Arduino Starter Kit example converted to johnny-five
- * Project 10  - Zoetrope
- *
- * Arduino Starter Kit:
- * http://www.arduino.cc/starterKit
- *
- * Examples:
- * https://github.com/arduino/Arduino/tree/master/build/shared/examples/10.StarterKit
- */
-
 import five from 'johnny-five'
+
+import mqtt from 'mqtt'
+
+const client = mqtt.connect('mqtt://192.168.78.96:3306')
+
+const topic = 'arduino3'
+
 var board = new five.Board();
 
 var motorSpeed = 0;
 var goForward = true;
 var isRunning = false;
+var directionSwitch, onOffSwitch, motor, potPin;
 
 board.on('ready', function() {
-  var directionSwitch = new five.Button(4);
-  var onOffSwitch = new five.Button(5);
-  var motor = new five.Motor([9, 3, 2]);
-  var potPin = new five.Sensor({
+  directionSwitch = new five.Button(4);
+  onOffSwitch = new five.Button(5);
+  motor = new five.Motor([9, 3, 2]);
+  potPin = new five.Sensor({
     pin: 'A0',
     freq: 250
-  });
+  })
+});
 
-  // allow command line access
-  board.repl.inject({
-    onOffSwitch: onOffSwitch,
-    directionSwitch: directionSwitch,
-    potPin: potPin,
-    motor: motor
-  });
+client.on('message', function (topicR, message) {
 
-  // Listen for onOffSwitch 'press' event
-  // toggle `isRunning` and start/stop motor
-  onOffSwitch.on('press', function() {
-    isRunning = !isRunning;
+  const messageStr = message.toString()
+  console.log(messageStr)
 
-    if (isRunning) {
-      motorStart();
-    } else {
-      motorStop()
+  if(messageStr != null){
+
+    // allow command line access
+    board.repl.inject({
+      onOffSwitch: onOffSwitch,
+      directionSwitch: directionSwitch,
+      potPin: potPin,
+      motor: motor
+    });
+
+    motor.start()
+    console.log('Motor started')
+
+    setTimeout(() => {
+      motor.stop()
+      console.log('Motor stopped')
+    }, 5000);
+
+    
+    // Listen for onOffSwitch 'press' event
+    // toggle `isRunning` and start/stop motor
+    onOffSwitch.on('press', function() {
+      isRunning = !isRunning;
+
+      if (isRunning) {
+        motorStart();
+      } else {
+        motorStop()
+      }
+    });
+
+    // Listen for directionSwitch 'press' event
+    // toggle `goForward`
+    // set isRunning to true (as this will start the motor)
+    // call motorForward() or motorReverse()
+    directionSwitch.on('press', function() {
+      goForward = !goForward;
+      if (!isRunning) isRunning = true;
+
+      if (goForward) {
+        motorForward();
+      } else {
+        motorReverse();
+      }
+    });
+
+    // Listen to 'data' event on the Potentiometer
+    // If the motor isn't running - get out
+    // As the motor is running, set `motorSpeed`
+    // Send the new speed through to a running motor
+    potPin.on('data', function() {
+      if (!isRunning) return;
+      motorSpeed = this.value / 4;
+
+      if (goForward) {
+        motorForward(motorSpeed);
+        console.log('Motor speed:', motorSpeed);
+      } else {
+        motorReverse(motorSpeed);
+        console.log('Motor speed:', motorSpeed);
+      }
+    });
+
+    /**
+     * motorStart
+     * Start the motor
+     */
+
+    function motorStart() {
+      motor.start(motorSpeed);
     }
-  });
 
-  // Listen for directionSwitch 'press' event
-  // toggle `goForward`
-  // set isRunning to true (as this will start the motor)
-  // call motorForward() or motorReverse()
-  directionSwitch.on('press', function() {
-    goForward = !goForward;
-    if (!isRunning) isRunning = true;
+    /**
+     * motorForward
+     * Set the direction of the motor
+     * Use `speed` if passed in, or default `motorSpeed`
+     * @param  {Number} speed
+     */
 
-    if (goForward) {
-      motorForward();
-    } else {
-      motorReverse();
+    function motorForward(speed) {
+      motorSpeed = speed || motorSpeed;
+      motor.forward(motorSpeed);
     }
-  });
 
-  // Listen to 'data' event on the Potentiometer
-  // If the motor isn't running - get out
-  // As the motor is running, set `motorSpeed`
-  // Send the new speed through to a running motor
-  potPin.on('data', function() {
-    if (!isRunning) return;
-    motorSpeed = this.value / 4;
+    /**
+     * motorReverse
+     * Set the direction of the motor
+     * Use `speed` if passed in, or default `motorSpeed`
+     * @param  {Number} speed
+     */
 
-    if (goForward) {
-      motorForward(motorSpeed);
-    } else {
-      motorReverse(motorSpeed);
+    function motorReverse(speed) {
+      motorSpeed = speed || motorSpeed;
+      motor.reverse(motorSpeed);
     }
-  });
 
-  /**
-   * motorStart
-   * Start the motor
-   */
+    /**
+     * motorStop
+     * Stop the motor
+     */
 
-  function motorStart() {
-    motor.start(motorSpeed);
-  }
-
-  /**
-   * motorForward
-   * Set the direction of the motor
-   * Use `speed` if passed in, or default `motorSpeed`
-   * @param  {Number} speed
-   */
-
-  function motorForward(speed) {
-    motorSpeed = speed || motorSpeed;
-    motor.forward(motorSpeed);
-  }
-
-  /**
-   * motorReverse
-   * Set the direction of the motor
-   * Use `speed` if passed in, or default `motorSpeed`
-   * @param  {Number} speed
-   */
-
-  function motorReverse(speed) {
-    motorSpeed = speed || motorSpeed;
-    motor.reverse(motorSpeed);
-  }
-
-  /**
-   * motorStop
-   * Stop the motor
-   */
-
-  function motorStop() {
-    motor.stop();
+    function motorStop() {
+      motor.stop();
+    }
   }
 });
+
+client.on('connect', function () {
+  client.subscribe(topic, function (err) {
+    if (!err) {
+      console.log('Subscribed to:', topic)
+    }
+  })
+})
