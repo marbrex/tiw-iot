4f62b75e437ebabbfe4183d4e4469055d07e1c90
Castaldo Laetitia <laetitia.castaldo@etu.univ-lyon1.fr>
2023-06-22 14:46:07 +0200

    Derniere modif

--- arduino_LCD/mqtt-client-sub.js
+++ arduino_LCD/mqtt-client-sub.js
@@ -5,8 +5,9 @@ const board = new five.Board()
 const client = mqtt.connect('mqtt://192.168.78.96:3306')
 
 const topic = 'arduino2'
+const topic2 = "arduino4"
 
-var lcd, servo, greenLight, redLight
+var lcd, servo, greenLight, redLight, ledPin, irSensor, compteur
 
 board.on('ready', function () {
   // Déclaration de l'écran LCD
@@ -20,6 +21,9 @@ board.on('ready', function () {
   greenLight = new five.Led(13)
   redLight = new five.Led(7)
 
+  ledPin = new five.Led(10)
+  irSensor = new five.Sensor("A5")
+
   //Déclaration du moteur pas à pas
   servo = new five.Servo({
     pin: 9,
@@ -32,6 +36,31 @@ board.on('ready', function () {
   servo.to(100)
   redLight.on()
 
+  ledPin.on()
+
+  compteur = 0
+
+  // Événement de lecture du capteur infrarouge
+  irSensor.on("change", function() {
+    const sensorValue = this.value;
+    console.log("Valeur du capteur :", sensorValue);
+
+    // Si un objet est détecté et suffisamment de temps s'est écoulé depuis la dernière détection
+    if (sensorValue > 200) {
+      compteur++;
+      if (compteur > 4) {
+        console.log("Bagage détecté");
+        compteur = 0;
+
+        // Publier le message MQTT "stop"
+        client.publish(topic2, "stop");
+      }
+    } else {
+      compteur = 0;
+      // Extinction de la LED infrarouge
+      //mqttClient.publish(topic, "1");
+    }
+  });
 })
 
 client.on('message', function (topicR, message) {
@@ -44,8 +73,19 @@ client.on('message', function (topicR, message) {
   const data = JSON.parse(messageStr);
 
   // Récupération des valeurs d'aéroport et de tapis
-  const airport = data[0].AirportName;
-  const tapis = data[0].Tapis;
+
+  let airport = ""
+  let tapis = ""
+
+  if(data[0] != undefined){
+    airport = data[0].AirportName;
+    tapis = data[0].Tapis;
+  } else {  
+    console.log("Erreur de parsing");
+    airport = "erreur";
+    tapis = "erreur";
+  }
+  
 
   console.log("Aéroport :", airport);
   console.log("Tapis :", tapis);
@@ -72,7 +112,7 @@ client.on('message', function (topicR, message) {
     } else if (tapis == 2) { // LEFT
       destination = airport
       servo.to(400)
-    } else if (tapis == 3) { // DISPUTE
+    } else if (tapis =="erreur") { // DISPUTE
       destination = airport
       servo.to(600)
     }
